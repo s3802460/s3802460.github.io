c823b05f6431708367f80c0031512c39727e57b2
Phu Nguyen <57164905+s3802460@users.noreply.github.com>
2021-05-05 11:10:43 +0700

    update number

--- JavaScript/register.js
+++ JavaScript/register.js
@@ -1,7 +1,3 @@
-function numbertype(evt){
-    var charCode = (evt.which) ? evt.which : evt.keyCode
-    return !(charCode > 31 && (charCode < 48 || charCode > 57));
-}
 function hidetype(){
     document.getElementById('Bussinesstype').style.display ='none';
 }
