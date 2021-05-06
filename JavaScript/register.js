b88abbb9777866297d29115479717ac989df8ba8
Phu Nguyen <57164905+s3802460@users.noreply.github.com>
2021-05-06 15:44:32 +0700

    update validate

--- JavaScript/register.js
+++ JavaScript/register.js
@@ -1,29 +1,33 @@
-/*Hide and Show div */
+// Hide and Show div 
 function hidetype(){
     document.getElementById('Bussinesstype').style.display ='none';
 }
 function showtype(){
     document.getElementById('Bussinesstype').style.display = 'block';
 }
-/*End of Hide and Show div */
 
-/*Check Retype Password */
+//Check Retype Password 
 var check = function() {
     if (document.getElementById("pass").value ==
         document.getElementById("confirmpass").value) {
-        document.getElementById("validmsg").style.color = 'green';
-        document.getElementById("validmsg").innerHTML = 'Passwords Match';
+        document.getElementById("correctPassword").style.color = 'green';
+        document.getElementById("correctPassword").innerHTML = 'Passwords Match';
     } else {
-        document.getElementById("validmsg").style.color = 'red';
-        document.getElementById("validmsg").innerHTML = 'Passwords Mismatch';
+        document.getElementById("correctPassword").style.color = 'red';
+        document.getElementById("correctPassword").innerHTML = 'Passwords Mismatch';
     }
 }
-/*End of Check Retype Password */
 
-/*Validate Phone Number */
+//Validate Phone Number 
 var validatePhone = function() {
-    phonePattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if((document.getElementById("phone").value.match(phonePattern))) {
+    phonePattern = /^[0-9]\d{8}$|^[0-9]\d{9}$|^[0-9]\d{10}$/;
+    phonePattern2 = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    phonePattern3 = /^\(?([0-9]{1})\)?[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})$/;
+    phonePattern4 = /^\(?([0-9]{1})\)?[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})$/;
+    phonePattern5 = /^\(?([0-9]{1})\)?[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{1})$/;
+    if(document.getElementById("phone").value.match(phonePattern) || document.getElementById("phone").value.match(phonePattern2)
+        || document.getElementById("phone").value.match(phonePattern3) || document.getElementById("phone").value.match(phonePattern4)
+        || document.getElementById("phone").value.match(phonePattern5)) {
         document.getElementById("validPhone").style.color = 'green';
         document.getElementById("validPhone").innerHTML = 'Valid Phone Number';
         return true;
@@ -33,10 +37,20 @@ var validatePhone = function() {
         return false;
     }
 };
-/*End of Validate Phone Number */
 
-/*Validate Email */
+//Validate Email
 var validateEmail = function() {
+    emailPattern = /^\(?[a-z]?[A-Z]){3,})$/;
+    if (document.getElementById("email").value.match(emailPattern)) {
+        document.getElementById("validEmail").style.color = 'green';
+        document.getElementById("validEmail").innerHTML = 'Valid Email';
+    } else {
+        document.getElementById("validEmail").style.color = 'red';
+        document.getElementById("validEmail").innerHTML = 'Not a Valid Email';
+    }
+};
+
+//Validate Password
+var validatePassword = function(){
 
 };
-/*End of Validate Email */
