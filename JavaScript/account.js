bffdc8812b449a06a03490750e8fc2364a850111
Phu Nguyen <57164905+s3802460@users.noreply.github.com>
2021-05-08 16:10:56 +0700

    complete login

--- JavaScript/account.js
+++ JavaScript/account.js
@@ -1,22 +1,34 @@
 var myStorage = window.sessionStorage;
-let accounts = [{username:'admin', pass:'admin123!', email:'admin@gmail.com'}];
+let accounts = [{username:'admin', pass:'Admin123!', phone:'123456789', email:'admin@gmail.com', avatar: "https://s3.zerochan.net/Berserker.%28Kiyohime%29.240.2118242.jpg"},
+                {username:'user2', pass:'user123!', phone:'323425', email:'user2@gmail.com', avatar:"https://s3.zerochan.net/Hoshimachi.Suisei.240.2989574.jpg"},
+                {username:'user3', pass:'user123!', phone:'6345645', email:'user3@gmail.com'}];
 myStorage.setItem("account", accounts);
+
+function redirect(url) {
+    window.location.replace(url);    
+  }
 
 //check login account
 var checkLogin = function(event){
     //event.preventDefault();
     let uname = document.getElementById("Uname").value;
     let pass = document.getElementById("Pass").value;
-    if(uname == "admin" && pass == "Admin123!"){
-        myStorage.setItem("user", {username:'admin', pass:'admin123!', email:'admin@gmail.com'});
-        alert("Login admin");
-        //window.location = "../account/myaccount.html";
-       // location.replace("https://vnexpress.net");
-        window.location.replace("https://vnexpress.net");
-        
-    } else {
-        myStorage.setItem("user", undefined);
-        document.getElementById("incorrectAccount").style.color = "red";
-        document.getElementById("incorrectAccount").innerHTML = "Incorrect Username or Password";
+
+    let loginUser = accounts.find(user => user.username === uname);
+
+    if(loginUser && loginUser.pass === pass) {
+        myStorage.setItem("user", JSON.stringify(loginUser));
+        redirect("../account/myaccount.html");
+        return false;        
+    }else{
+        loginFail();
+        return false;    
     }
+    
+};
+
+var loginFail = function(){
+    myStorage.setItem("user", undefined);
+    document.getElementById("incorrectAccount").style.color = "red";
+    document.getElementById("incorrectAccount").innerHTML = "Incorrect Username or Password"; 
 };
