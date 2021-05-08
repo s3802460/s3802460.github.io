f96cc0afe0881a20d8363570c816a00cf80745f1
Phu Nguyen <57164905+s3802460@users.noreply.github.com>
2021-05-08 18:44:45 +0700

    update

--- JavaScript/mall/counter.js
+++ JavaScript/mall/counter.js
@@ -2,8 +2,9 @@ var counter = function(){
     let minlength=50;
     let maxlength = 500;
     let currentlength = document.getElementById("messagebox").value.length;
-    if (currentlength<50) {
-        document.getElementById("countChars").innerHTML = 'Please type ' + (50 - currentlength) + " more characters to meet the minimum length";
+    if (currentlength<minlength) {
+        document.getElementById("countChars").innerHTML = 'Please type ' + 
+        (50 - currentlength) + " more characters to meet the minimum length";
     } else if (currentlength >= maxlength){
         document.getElementById("countChars").innerHTML = 'You have reached the maximum amount characters ';
     } else {
